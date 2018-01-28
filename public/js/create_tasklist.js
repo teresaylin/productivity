7baca4f3b590f0703b72a0c6a2db6da025f89881
tlin15 <tlin15@mit.edu>
2018-01-28 16:10:51 -0500

    Add hidden input elements for tasks that can be sent by POST request

--- public/js/create_tasklist.js
+++ public/js/create_tasklist.js
@@ -4,19 +4,27 @@ function addTask() {
   if (task === '') {
     alert("Cannot add empty task");
   } else {
-    var newtaskLI = "<li>" + task + "<span class='close'>\u00D7</span>" + "</li>";
+    var numTasks = $('#tasklist').children().length;
+    var newtaskLI = "<li id='task" + numTasks + "'>" + task + "<span class='close'>\u00D7</span>" + "</li>";
     $('#tasklist').append(newtaskLI);
+
+    // add a hidden input element so that tasks can be sent via POST request
+    var newtaskInput = "<input type='hidden' value='" + task + "' name='task" + numTasks + "' id='inputtask" + numTasks + "'>";
+    $('#taskform').append(newtaskInput);
   }
 }
 
 // Toggle "checked" class for all <li> elements when clicked
+// Toggle "checked" class for corresponding hidden <input> element
 $(document).on("click", "li", function() {
   $(this).toggleClass("checked");
+  var inputId = 'input' + $(this).attr('id');   //e.g. #inputtask11
+  $('#' + inputId).toggleClass("checked");  
 });
 
 // Make tasks disappear when the 'x' button is clicked
+// Add "removed" class to <li> task element that was removed
 $(document).on("click", ".close", function() {
-  console.log($(this).parent());
   $(this).parent().css("display", "none");;
   $(this).parent().addClass("removed");
 });
