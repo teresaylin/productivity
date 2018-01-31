ceeb111c9af8ef0ec63149851a62e86a5da226ed
tlin15 <tlin15@mit.edu>
2018-01-31 16:51:13 -0500

    dropdown menu for selecting an environment

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,4 @@
+// side nav menu functions
 function openNav() {
   document.getElementById('mySidenav').style.width = '250px';
   document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
@@ -7,3 +8,17 @@ function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
   document.body.style.backgroundColor = 'white';
 }
+
+// dropdown menu functions
+function drop() {
+  $('#myDropdown').toggleClass('show');
+}
+
+$(window).click(function(event) {
+  if(event.target.className!=='dropbtn') {
+    if($('#myDropdown').hasClass('show')) {
+      console.log('outside of target');
+      $('#myDropdown').removeClass('show');
+    }
+  }
+});
