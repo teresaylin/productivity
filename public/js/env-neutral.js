68d1efdfcd95464adc40253ce2477bcdf6f7d740
tlin15 <tlin15@mit.edu>
2018-03-29 17:58:24 -0400

    Neutral env - removed countdown, added taskboxes, added checkboxes

--- public/js/env-neutral.js
+++ public/js/env-neutral.js
@@ -15,8 +15,44 @@ $(function() {
     }
   });
 
-  // check every 10 seconds to see if the clock is still ticking
-  setInterval(checkClock, 10000);
+  // position tasks and their checkboxes
+  var taskWidth = $('.tasksOnBar').width();
+  var checkWidth = $('.checkbox').outerWidth(true); //includes margin and padding
+  var checkHeight = $('.checkbox').outerHeight(true);
+  var taskLeft = ($(window).width() - taskWidth - checkWidth)/2 + checkWidth;
+  var checkLeft = ($(window).width() - taskWidth - checkWidth)/2;
+
+  $('.tasksOnBar').each(function(index, element) {
+    var children = $(element).children();
+    var taskId = $(element).attr('id').slice(4);
+    var taskHeight = $('.tasksOnBar').outerHeight(true);
+    var complete = $(children[2]).text();
+    var date = $(children[1]).text();
+    var now = new Date().getTime();
+    var diff = new Date(date).getTime() - now;
+    var informaldate = $(children[3]);
+    var date_formatted = moment(date).format('h:mm a MMM D');
+    var separation = 100;
+    var startingTop = 100;
+
+    $(element).css('top', separation*index + startingTop);
+    $(element).css('left', taskLeft);
+    informaldate.text(date_formatted);
+    $('#'+'check'+taskId).css('top', separation*index + startingTop + (taskHeight - checkHeight)/2);
+    $('#'+'check'+taskId).css('left', checkLeft);
+
+    if(diff <= 0) {
+      $(element).css('text-decoration', 'line-through');
+      $(element).css('font-style', 'italic');
+    } else if(complete == 'true') {
+      $(element).css('text-decoration', 'line-through');
+      $(element).css('font-style', 'italic');
+      $(element).css('background-color', '#6ed06e');
+    }
+  });
+
+  // TODO if all tasks are completed
+
 });
 
 // Once user selects a task, starts the clock
@@ -26,66 +62,27 @@ $(document).on("click", ".taskobj", function() {
   var date = $(children[1]).text();
   $('.dropdownTasks').hide();
 
-  // position the 'Working on' text
   $('#workingOn').text('Working on: ' + task);
-  var timerWidth = $('#workingOn').width();
-  var timerHeight = $('#workingOn').height();
-  $('#workingOn').css('top', ($(window).height() - timerHeight)/2 - 200);
-  $('#workingOn').css('left', ($(window).width() - timerWidth)/2);
-  $('#workingOn').show();
-  
-  // position checkmark button
-  var checkmarkSide = $('#finishedCircleDiv').width();
-  $('#finishedCircleDiv').css('top', ($(window).height() - checkmarkSide)/2 + 50);
-  $('#finishedCircleDiv').css('left', ($(window).width() - checkmarkSide)/2);
-  $('#finishedCircleDiv').show();
 
-  // implement countdown
-  var deadline = new Date(date).getTime();
-  intervalId = setInterval(function() { 
-    startClock(deadline);
-  }, 1000);
+  // mark selected task as 'current', highlight current task, and show tasks
+  var foundCurrent = false;   // in case there are 2 identical tasks
+  $('.tasksOnBar').each(function(index, element) {
+    var children = $(element).children();
+    var sideTask = children[0];
+    var bgcolor = $(element).css('background-color');
+    var taskId = $(element).attr('id').slice(4);    // task11 -> 11
+    if($(sideTask).text() === task && !foundCurrent && bgcolor !== 'rgb(110, 208, 110)') {
+      $(element).addClass('current');
+      foundCurrent = true;
+    } else {
+      $(element).removeClass('current');
+    }
+    $(element).css('display', 'inline-flex'); // puts deadline and task on the same line
+    $(element).show();
+    $('#'+'check'+taskId).show();
+  });
 });
 
-// Display remaining time left
-function startClock(deadline) {
-  var now = new Date().getTime();
-  var diff = deadline - now;
-  var days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+// TODO when user clicks on a different task, switch to that task
 
-  // if timer has run out
-  if(diff <= 0) {
-    $('#days').text(0);
-    $('#hours').text(0);
-    $('#minutes').text(0);
-    $('#seconds').text(0);
-  } else {
-    $('#days').text(days);
-    $('#hours').text(hours);
-    $('#minutes').text(minutes);
-    $('#seconds').text(seconds);
-  }
-  // make timer red if < 10 minutes remaining
-  if(diff <= 600000) {
-    $('.time').css('color', '#ff0000');
-  }
-  // position entire div in vertical center
-  var timerWidth = $('#countdown').width();
-  var timerHeight = $('#countdown').height();
-  // $('#countdown').css('top', ($(window).height() - timerHeight)/2 - 100);
-  $('#countdown').css('top', 0)
-  $('#countdown').css('left', ($(window).width() - timerWidth)/2);
-  $('#countdown').show();
-}
-
-// Check to see if timer has expired, and if so, show the dropdown menu again
-function checkClock() {
-  if($('#days').text() === '0' && $('#hours').text() === '0' && $('#minutes').text() === '0' && $('#seconds').text() === '0' && intervalId !== null) {
-    clearInterval(intervalId);
-    intervalId = null;
-    location.reload();
-  }
-}
+// TODO when user checks off a task
